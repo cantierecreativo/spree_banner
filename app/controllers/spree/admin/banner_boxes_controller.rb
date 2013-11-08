ca01304da421cd40044ddc8d864da95e5c9c61f1
nebirhos <me@nebirhos.com>
2013-11-08 18:27:46 +0100

    Increase per page banners in index

--- app/controllers/spree/admin/banner_boxes_controller.rb
+++ app/controllers/spree/admin/banner_boxes_controller.rb
@@ -1,15 +1,15 @@
 module Spree
   module Admin
     class BannerBoxesController < ResourceController
-      
+
       def index
         respond_with(@collection)
       end
-      
+
       def show
         redirect_to( :action => :edit )
       end
-      
+
       def update
         @banner_box.enhance_settings
         super
@@ -26,23 +26,24 @@ module Spree
 
         respond_with(@new) { |format| format.html { redirect_to edit_admin_banner_box_url(@new) } }
       end
-      
+
       protected
+
       def find_resource
         Spree::BannerBox.find(params[:id])
       end
-      
+
       def location_after_save
          edit_admin_banner_box_url(@banner_box)
       end
-      
+
       def collection
         return @collection if @collection.present?
         params[:q] ||= {}
         params[:q][:s] ||= "position asc"
 
         @search = super.ransack(params[:q])
-        @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+        @collection = @search.result.page(params[:page]).per(50)
       end
     end
   end
